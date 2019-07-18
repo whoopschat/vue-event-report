20c2316b54138271eba5888e48a95fca4e7485ef
梁马勇 <liangmayong@qutoutiao.net>
2019-07-18 20:45:15 +0800

    opt position func

--- src/_exposure.js
+++ src/_exposure.js
@@ -12,42 +12,82 @@ function __getViewportSize() {
     }
 }
 
-function _getAbsoluteScroll(element) {
-    let scrollTop, scrollLeft;
-    scrollTop = 0;
-    scrollLeft = 0;
-    while (element = element.parentElement) {
-        scrollTop += -(element.scrollTop || 0);
-        scrollLeft += - (element.scrollLeft || 0);
+function _getStyle(element, attr) {
+    try {
+        return element.currentStyle ? element.currentStyle[attr] : getComputedStyle(element, false)[attr];
+    } catch (error) {
+    }
+}
+
+function _getScroll(element) {
+    let scrollTop = 0;
+    let scrollLeft = 0;
+    try {
+        if (element && _getStyle(element, 'position') != 'fixed') {
+            scrollTop += -(element.scrollTop || 0);
+            scrollLeft += - (element.scrollLeft || 0);
+            let next = () => {
+                element = element.parentElement;
+                return element && _getStyle(element, 'position') != 'fixed';
+            }
+            while (next()) {
+                scrollTop += -(element.scrollTop || 0);
+                scrollLeft += - (element.scrollLeft || 0);
+            }
+        }
+    } catch (error) {
     }
     return { scrollTop, scrollLeft };
 }
 
-function _getAbsoluteLocation(element) {
+function _getElement(el) {
+    if (typeof el === 'string') {
+        try {
+            el = document.documentElement.querySelector(el);
+        } catch (error) {
+        }
+    }
+    return el;
+}
+
+function _getPosition(el) {
+    let element = _getElement(el);
     if (arguments.length != 1 || element == null) {
         return null;
     }
-    var { scrollTop, scrollLeft } = _getAbsoluteScroll(element);
-    var offsetTop = element.offsetTop;
-    var offsetLeft = element.offsetLeft;
-    var offsetWidth = element.offsetWidth;
-    var offsetHeight = element.offsetHeight;
-    while (element = element.offsetParent) {
-        offsetTop += element.offsetTop;
-        offsetLeft += element.offsetLeft;
+    let scrollTop = 0;
+    let scrollLeft = 0;
+    let offsetTop = 0;
+    let offsetLeft = 0;
+    let offsetWidth = 0;
+    let offsetHeight = 0;
+    try {
+        let scroll = _getScroll(element);
+        scrollTop = scroll.scrollTop || 0;
+        scrollLeft = scroll.scrollLeft || 0;
+        offsetTop = element.offsetTop || 0;
+        offsetLeft = element.offsetLeft || 0;
+        offsetWidth = element.offsetWidth || 0;
+        offsetHeight = element.offsetHeight || 0;
+        while (element = element.offsetParent) {
+            offsetTop += element.offsetTop;
+            offsetLeft += element.offsetLeft;
+        }
+    } catch (error) {
     }
     return {
         top: offsetTop + scrollTop,
         left: offsetLeft + scrollLeft,
         width: offsetWidth,
-        height: offsetHeight
-    };
+        height: offsetHeight,
+    }
 }
 
 function _checkVisible(div) {
     try {
         let { w, h } = __getViewportSize();
-        let { top, left, width, height } = _getAbsoluteLocation(div);
+        let { top, left, width, height } = _getPosition(div);
+        console.log({ top, left, width, height });
         return width > 0 && height > 0 && top + height >= 0 && top < h && left + width >= 0 && left < w;
     } catch (error) {
     }
