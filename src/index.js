f22728620bb8e97f4eaa025e62433244bb52bd54
iqudoo <iqudoo@qq.com>
2022-05-19 16:13:20 +0800

    v2.0.0

--- src/index.js
+++ src/index.js
@@ -3,42 +3,31 @@ import { _initReport, _reportEvent } from './_report';
 let _handler = null;
 let _installed = false;
 
-function create(vue) {
-    _initReport(vue, (...params) => {
-        _handler && _handler(...params);
-    });
-    return {
-        setReportHandler: (handler) => {
-            if (handler && typeof handler === 'function') {
-                _handler = handler;
-            }
-        },
-        reportEvent: (event, data) => {
-            _reportEvent(event, data);
-        }
-    }
-}
-
-function setGlobal(key, value, vue) {
-    if (key && value && typeof key == 'string') {
-        if (typeof window !== 'undefined') {
-            window[key] = value;
-        }
-        if (vue && vue.prototype) {
-            vue.prototype[key] = value;
-        }
-    }
-}
-
 function install(vue) {
     if (_installed || !vue) {
         return;
     }
-    let vreport = create(vue);
-    setGlobal("VReport", vreport, vue);
+    _initReport(vue, (...params) => {
+        _handler && _handler(...params);
+    });
+    if (vue && vue.prototype) {
+        vue.prototype.$reportEvent = reportEvent;
+    }
     _installed = true;
 }
 
+function setReportHandler(handler) {
+    if (handler && typeof handler === 'function') {
+        _handler = handler;
+    }
+}
+
+function reportEvent(event, data) {
+    _reportEvent(event, data);
+}
+
 module.exports = {
-    install
+    install,
+    setReportHandler,
+    reportEvent,
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,29 +1,22 @@
 declare module "vue-event-report" {
 
+    /**
+     * 初始化
+     * @param vue vue
+     */
     function install(vue: any): void;
 
-    interface VReport {
+    /**
+     * 设置上报事件处理器
+     * @param handler 上报事件处理器
+     */
+    function setReportHandler(handler: ({ event: string, data: any }) => {}): void;
 
-        /**
-         * 设置上报事件处理器
-         * @param handler 上报事件处理器
-         */
-        setReportHandler(handler: ({ event: string, data: any }) => {}): void;
+    /**
+     * 上报事件
+     * @param event 事件
+     * @param data 数据
+     */
+    function reportEvent(event: string, data: any): void;
 
-        /**
-         * 上报事件
-         * @param event 事件
-         * @param data 数据
-         */
-        reportEvent(event: string, data: any): void;
-
-    }
-
-}
-
-
-declare var VReport: import("vue-event-report").VReport;
-
-declare interface Window {
-    VReport: import("vue-event-report").VReport;
 }
